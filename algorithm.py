7d7f147543f22adc340632b14935570cad5aba27
kevintaw <benimaru_tao@163.com>
2015-03-22 22:48:52 +0800

    extended assignment operator

--- algorithm.py
+++ algorithm.py
@@ -1,1 +1,22 @@
 __author__ = 'tnb'
+
+
+
+
+count = 5
+print(count)
+count += 5
+print(count)
+count = count + 5
+print(count)
+
+alpha = [1, 2, 3]
+print(alpha)
+beta = alpha
+print(beta)
+beta += [4, 5]
+print(beta)
+beta = beta + [6, 7]
+print(beta)
+print(alpha)
+
